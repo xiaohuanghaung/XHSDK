fbc73108763f3146d466b03efbe071d91fbfa520
111 <18811574335>
2019-01-22 10:58:28 +0800

    创建Cocoapods测试工程

---
+++ Example/Pods/Target Support Files/Pods-XHSDK_Tests/Pods-XHSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XHSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XHSDK_Tests
+@end
---
+++ Example/Pods/Target Support Files/XHSDK/XHSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XHSDK : NSObject
+@end
+@implementation PodsDummy_XHSDK
+@end
